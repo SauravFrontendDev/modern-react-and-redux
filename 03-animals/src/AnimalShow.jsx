8a0353de66819b665925f236d5cfecae2b5ab182
Saurav kumar verma <sauravkumarverma22@gmail.com>
2023-09-26 01:39:44 +0530

    state + events + list

--- 03-animals/src/AnimalShow.jsx
+++ 03-animals/src/AnimalShow.jsx
@@ -1,7 +1,43 @@
 // import React from "react";
+import "./AnimalShow.css";
 
-const AnimalShow = () => {
-  return <div>Cow</div>;
+import { useState } from "react";
+import bird from "./assets/bird.svg";
+import cat from "./assets/cat.svg";
+import cow from "./assets/cow.svg";
+import dog from "./assets/dog.svg";
+import gator from "./assets/gator.svg";
+import heart from "./assets/heart.svg";
+import horse from "./assets/horse.svg";
+
+const svgMap = {
+  bird,
+  cat,
+  cow,
+  dog,
+  gator,
+  horse,
+};
+
+const AnimalShow = ({ name }) => {
+  const [count, setCount] = useState(1);
+  const handleClicks = () => {
+    setCount((c) => c + 1);
+  };
+  return (
+    <div onClick={handleClicks} className="animal-show">
+      <img className="animal" src={svgMap[name]} alt={name} />
+      <img
+        className="heart"
+        src={heart}
+        alt="heart"
+        style={{
+          width: 20 + 20 * count + "px",
+        }}
+      />
+      <h4>{name}</h4>
+    </div>
+  );
 };
 
 export default AnimalShow;
--- 03-animals/src/App.jsx
+++ 03-animals/src/App.jsx
@@ -1,18 +1,29 @@
 // import React from "react";
+
+import "./App.css";
 import { useState } from "react";
 import AnimalShow from "./AnimalShow";
 
+const getRandomAnimal = () => {
+  const animals = ["bird", "cat", "cow", "dog", "gator", "horse"];
+
+  return animals[Math.floor(Math.random() * animals.length)];
+};
+
+console.log(getRandomAnimal());
 const App = () => {
-  const [count, setCount] = useState(0);
+  const [animals, setAnimals] = useState([]);
+
   const handleClick = () => {
-    setCount((prev) => prev + 1);
+    setAnimals((a) => [...a, getRandomAnimal()]);
   };
   return (
-    <div>
+    <div className="app">
       <button onClick={handleClick}>Add Animal</button>
-      <div>
-        <h2>Count is : {count}</h2>
-        <AnimalShow />
+      <div className="animal-list">
+        {animals.map((a, i) => (
+          <AnimalShow name={a} key={i} />
+        ))}
       </div>
     </div>
   );
